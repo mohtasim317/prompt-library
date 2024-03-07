90fa006e165993ac25bd7ea705f6a9bb86286bf5
Mohtasim <mohtasim317@hotmail.com>
2024-03-07 13:03:43 -0500

    Added styling overlay

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import PromptList from "./components/PromptList/PromptList";
 
 function App() {
   return (
-    <>
-      <div>hello</div>
+    <div className="App">
+      <div className="NavRow">Nav Row Placeholder</div>
+      <div className="SideBar">Sidebar</div>
       <PromptList />
-    </>
+      <div className="Modal">Modal PlaceHolder</div>
+    </div>
   );
 }
 
