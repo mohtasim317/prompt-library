fbe54e23a219e5de91646b1cfeaf701f728b7446
Mohtasim <mohtasim317@hotmail.com>
2024-03-13 11:48:55 -0400

    Updated mock data to now have an id property. Updated mock data interface to have id field

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,6 +2,7 @@ import { MockDataType } from "../types";
 
 export const promptData: MockDataType[] = [
   {
+    id: 1,
     title: "Define my target audience’s needs to buy my coaching program",
     text: "I want you to write /'irresistible personal tweets' for me.\
 These tweets are characterized by being extremely original and unique. They always include one reference, fact, opinion, or experience by the...",
@@ -9,16 +10,19 @@ These tweets are characterized by being extremely original and unique. They alwa
   },
 
   {
+    id: 2,
     title: "prompt # 2",
     text: "prompt # 2 text",
     folder: "prompt # 2 platform",
   },
   {
+    id: 3,
     title: "prompt # 3",
     text: "prompt # 3 text",
     folder: "prompt # 3 platform",
   },
   {
+    id: 4,
     title: "prompt # 4",
     text: "prompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 textprompt # 4 text",
     folder: "prompt # 4 platform platform platform platform platform platform",
