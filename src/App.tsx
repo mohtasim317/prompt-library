0f6df529e5df845cec2673576b4c45dc8eae0117
Mohtasim <mohtasim317@hotmail.com>
2024-03-28 10:52:14 -0400

    Created and styled Modal pop-up and button

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import PromptList from "./components/PromptList/PromptList";
 import FolderBar from "./components/FolderBar/FolderBar";
 import PromptForm from "./components/PromptForm/PromptForm";
 import { PromptContextProvider } from "./PromptContext";
+import { useState } from "react";
+import Modal from "./components/Modal/Modal";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="App">
       <div className="NavRow">Nav Row Placeholder</div>
       <FolderBar />
       <PromptContextProvider>
         <>
-          <PromptList />
+          <PromptList setShowModal={setShowModal} />
           <PromptForm />
+          {showModal && <Modal setShowModal={setShowModal} />}
         </>
       </PromptContextProvider>
     </div>
--- src/components/PromptList/PromptList.tsx
+++ src/components/PromptList/PromptList.tsx
@@ -1,13 +1,16 @@
 import "./PromptList.css";
 import { promptData } from "../../constants/constants";
 import PromptTile from "../PromptTile/PromptTile";
+import { PromptListPropsType } from "../../types";
 
-function PromptList() {
+function PromptList({ setShowModal }: PromptListPropsType) {
   return (
     <div className="PromptList">
       <div className="ButtonBar">
         <button>New Prompt</button>
-        <button>Create New Dropdown</button>
+        <button onClick={() => setShowModal((prevState) => !prevState)}>
+          Create New Dropdown
+        </button>
       </div>
       {promptData.map((prompt) => {
         return <PromptTile {...prompt} />;
