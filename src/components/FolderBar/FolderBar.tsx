689c2852e8260d23bc39bef4d486389ce55b4046
Mohtasim <mohtasim317@hotmail.com>
2024-04-02 10:08:46 -0400

    Style Prompt Form to use grid

--- src/components/FolderBar/FolderBar.tsx
+++ src/components/FolderBar/FolderBar.tsx
@@ -14,29 +14,31 @@ function FolderBar() {
 
   return (
     <>
-      <div className="FolderBar">
+      <div className="FolderandDropDownSection">
         <div>
-          {folderList.length > 0 ? (
-            folderList.map((folder) => {
-              return <div>{folder}</div>;
-            })
-          ) : (
-            <div>No Folders Added</div>
+          <div>
+            {folderList.length > 0 ? (
+              folderList.map((folder) => {
+                return <div>{folder}</div>;
+              })
+            ) : (
+              <div>No Folders Added</div>
+            )}
+          </div>
+
+          <button onClick={() => setShowInputBox((prevState) => !prevState)}>
+            New Folder
+          </button>
+
+          {showInputBox && (
+            <input
+              type="text"
+              onChange={(event) => setInputFolderName(event.target.value)}
+              value={inputFolderName}
+              onKeyDown={handleKeyDown}
+            />
           )}
         </div>
-
-        <button onClick={() => setShowInputBox((prevState) => !prevState)}>
-          New Folder
-        </button>
-
-        {showInputBox && (
-          <input
-            type="text"
-            onChange={(event) => setInputFolderName(event.target.value)}
-            value={inputFolderName}
-            onKeyDown={handleKeyDown}
-          />
-        )}
       </div>
     </>
   );
