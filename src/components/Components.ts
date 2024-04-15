f4892aeee102c0c0c81270679130812dd50e4fb1
Mohtasim <mohtasim317@hotmail.com>
2024-04-15 10:38:07 -0400

    Created file to handle all component exports

---
+++ src/components/Components.ts
@@ -0,0 +1,7 @@
+export { default as DropDown } from "./DropDown/DropDown";
+export { default as DropdownTile } from "./DropdownTile/DropdownTile";
+export { default as FolderAndDropdownSection } from "./FolderAndDropdownSection/FolderAndDropdownSection";
+export { default as Modal } from "./Modal/Modal";
+export { default as PromptForm } from "./PromptForm/PromptForm";
+export { default as PromptList } from "./PromptList/PromptList";
+export { default as PromptTile } from "./PromptTile/PromptTile";
